0df896800da112199ebddfa0bea4c597148afec3
Dax Sorbito <daxsorbito@gmail.com>
2016-12-31 13:53:06 +0800

    feat: added more tweens

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,13 @@
                           {color: "#FFF"},
                           {color: "rgb(23, 184, 111)"}
                          );
-
-  var scene = new ScrollMagic.Scene({triggerElement: "#one", duration: 200, offset: -50})
+  var topNavBgTween = TweenMax.fromTo("#topNav", 1, { backgroundColor: "transparent" }, {backgroundColor: "black"});
+  var scene = new ScrollMagic.Scene({triggerElement: "#one", duration: 500, offset: -50})
                   .setTween(logoTween)
+                  .addIndicators({name: "first section"})
+                  .addTo(controller)
+  var scene = new ScrollMagic.Scene({triggerElement: "#one", duration: 200, offset: -50})
+                  .setTween(topNavBgTween)
                   .addIndicators({name: "first section"})
                   .addTo(controller)
 
