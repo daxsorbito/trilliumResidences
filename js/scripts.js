6ead1cb96914dd62af97b5f0e1a663b416595dae
Dax Sorbito <daxsorbito@gmail.com>
2016-12-31 21:19:48 +0800

    feat: more tweens on main menus

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,36 @@
 ! function(t) {
   "use strict";
+  const controller = new ScrollMagic.Controller();
 
-  var controller = new ScrollMagic.Controller();
+  const setSceneTween = (tween, sceneOptions) => {
+    new ScrollMagic.Scene(sceneOptions)
+    .setTween(tween)
+    .addIndicators({name: "first section"})
+    .addTo(controller)
+  }
 
-  var logoTween = TweenMax.fromTo(".logo-container", 1,
-                          {color: "#FFF"},
-                          {color: "rgb(23, 184, 111)"}
-                         );
-  var topNavBgTween = TweenMax.fromTo("#topNav", 1, { backgroundColor: "transparent" }, {backgroundColor: "black"});
-  var scene = new ScrollMagic.Scene({triggerElement: "#one", duration: 500, offset: -50})
-                  .setTween(logoTween)
-                  .addIndicators({name: "first section"})
-                  .addTo(controller)
-  var scene = new ScrollMagic.Scene({triggerElement: "#one", duration: 200, offset: -50})
-                  .setTween(topNavBgTween)
-                  .addIndicators({name: "first section"})
-                  .addTo(controller)
+  const setMenuTween = (activeIndex) => {
+    return TweenMax.to(`#menu li:nth-child(${activeIndex}) > a`, 1,
+                                {
+                                  color:"rgb(153,101,21)",
+                                  onStart: () => {
+                                    TweenMax.to("#menu li > a", 0, {color:"white"})
+                                  }})
+  }
+  // for div one trigger
+  const sceneOneOptions = {triggerElement: "#one", duration: 200, offset: 0};
+  const logoTween = TweenMax.to(".logo-container", 1, {color: "rgb(23, 184, 111)"});
+  const topNavBgTween = TweenMax.to("#topNav", 1, {backgroundColor: "black"});
+  const sectionHeader = TweenMax.to("#one  h2.text-primary", 1, {color:"orange"})
+  const setAllMenu = TweenMax.to("#menu li > a", 1, {color:"white"})
+
+  // for div two trigger
+  const sceneTwoOptions = {triggerElement: "#two", duration: 200, offset: 0};
+
+  setSceneTween(logoTween, sceneOneOptions);
+  setSceneTween(topNavBgTween, sceneOneOptions)
+  setSceneTween(sectionHeader, sceneOneOptions)
+  setSceneTween(setMenuTween(1), sceneOneOptions)
+  setSceneTween(setMenuTween(2), sceneTwoOptions)
 
 }(jQuery);
